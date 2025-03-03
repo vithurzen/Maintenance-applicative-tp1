b61fe902059afb333c1d54bcf3d59736fb6e88a3
vithurzen <bouchkhaf@gmail.com>
2025-03-03 15:13:08 +0100

    step7

--- src/main/java/fr/r6a06/Movie.java
+++ src/main/java/fr/r6a06/Movie.java
@@ -21,4 +21,28 @@ public class Movie {
     public String getTitle (){
         return _title;
     };
+
+    public double getCharge(int daysRented) {
+        return extracted(daysRented);
+    }
+
+    private double extracted(int daysRented) {
+        double result = 0.0;
+        switch (this.getPriceCode()) {
+            case REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
--- src/main/java/fr/r6a06/Rental.java
+++ src/main/java/fr/r6a06/Rental.java
@@ -9,7 +9,7 @@ public class Rental {
         _daysRented = daysRented;
     }
 
-    public int getDaysRented() {
+    public  int getDaysRented() {
         return _daysRented;
     }
 
@@ -17,24 +17,10 @@ public class Rental {
         return _movie;
     }
 
-    public double getCharge() {
-        double thisAmount = 0;
-        switch (this.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (this.getDaysRented() > 2)
 
-                    thisAmount += (this.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += this.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (this.getDaysRented() > 3)
-                    thisAmount += (this.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
+    public double getCharge() {
+        return _movie.getCharge(_daysRented);
     }
+
+
 }
